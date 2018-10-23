36ddb46a1328a70aef5046baaf860d4a6784087f
Martin Wåger <martinw@gmail.com>
2018-10-23 10:49:00 +0200

    Update Constants.cs

--- ConsoleResourceOwnerFlow/Constants.cs
+++ ConsoleResourceOwnerFlow/Constants.cs
@@ -4,10 +4,10 @@ namespace ConsoleResourceOwnerFlow
 {
     public class Constants
     {
-        public const string Authority = "https://login.comaround.com";
-        public const string SampleApi = "https://zero.comaround.com";
-        public const string Client = "zeroapi";
-        public const string Secret = "E2AC8682-7258-4922-8041-1C576E5C9DD2";
-        public const string Scope = "zeroapi";
+        public const string Authority = "";
+        public const string SampleApi = "";
+        public const string Client = "";
+        public const string Secret = "";
+        public const string Scope = "";
     }
 }
