e5a60827f1e0579fc38f0619a88a6be1cbaaae3c
Martin Wåger <martin.wager@comaround.com>
2018-10-23 10:45:23 +0200

    Dictionary

--- ConsoleResourceOwnerFlow/Program.cs
+++ ConsoleResourceOwnerFlow/Program.cs
@@ -2,6 +2,7 @@
 using IdentityModel.Client;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,35 +11,47 @@ namespace ConsoleResourceOwnerFlow
 {
     internal class Program
     {
+        private static Dictionary<string, (TokenResponse, DateTime)> _tokens = new Dictionary<string, (TokenResponse, DateTime)>();
         private static async Task Main()
         {
             Console.Title = "Console ResourceOwner Flow";
-            var response = await RequestTokenAsync();
-            Show(response);
-            Console.ReadLine();
-            await CallServiceAsync(response.AccessToken);
-        }
-
-        private static async Task<TokenResponse> RequestTokenAsync()
-        {
-            var disco = await DiscoveryClient.GetAsync(Constants.Authority);
-            if (disco.IsError) throw new Exception(disco.Error);
-
-            var client = new TokenClient(disco.TokenEndpoint, Constants.Client, Constants.Secret);
-
-            // idsrv supports additional non-standard parameters 
-            // that get passed through to the user service
-            //var optional = new
-            //{
-            //    acr_values = "tenant:custom_account_store1 foo bar quux"
-            //};
-
-            //return await client.RequestResourceOwnerPasswordAsync("bob", "bob", "api1 api2.read_only", optional);
             Console.WriteLine("Enter username:");
             var username = Console.ReadLine();
             Console.WriteLine("Enter password:");
             var password = Console.ReadLine();
-            return await client.RequestResourceOwnerPasswordAsync(username, password, Constants.Scope);
+            var response = await GetTokenAsync(username, password);
+            //response = await GetTokenAsync(username, password);
+            //ShowToken(response);
+            //Console.ReadLine();
+            await CallServiceAsync(response.AccessToken);
+            Console.ReadLine();
+        }
+
+        private static bool IsExpired(DateTime date)
+        {
+            return date < DateTime.UtcNow;
+        }
+
+        private static async Task<TokenResponse> GetTokenAsync(string username, string password)
+        {
+            var key = $"{username}-{password}";
+            if (_tokens.TryGetValue(key, out var tokenAndDate))
+            {
+                if (!IsExpired(tokenAndDate.Item2))
+                {
+                    return tokenAndDate.Item1;
+                }
+            }
+            var tokenEndpoint = $"{Constants.Authority}/connect/token";
+            var client = new TokenClient(tokenEndpoint, Constants.Client, Constants.Secret);
+            var token = await client.RequestResourceOwnerPasswordAsync(username, password, Constants.Scope);
+            if (token.IsError)
+            {
+                throw new Exception($"Token error from {tokenEndpoint}: {token.Error}");
+            }
+            var tokenExpiry = DateTime.UtcNow.AddSeconds(token.ExpiresIn - 60); // Subtract 60s to allow for call time}
+            _tokens.Add(key, (token, tokenExpiry));
+            return token;
         }
 
         private static async Task CallServiceAsync(string token)
@@ -50,13 +63,13 @@ namespace ConsoleResourceOwnerFlow
             };
             client.SetBearerToken(token);
             var response = await client.GetAsync("/api/v1/whoami/");
-            var result  = await response.Content.ReadAsStringAsync();
+            var result = await response.Content.ReadAsStringAsync();
             Console.WriteLine("Whoami:");
             Console.WriteLine(JObject.Parse(result));
         }
 
 
-        private static void Show(TokenResponse response)
+        private static void ShowToken(TokenResponse response)
         {
             if (!response.IsError)
             {
